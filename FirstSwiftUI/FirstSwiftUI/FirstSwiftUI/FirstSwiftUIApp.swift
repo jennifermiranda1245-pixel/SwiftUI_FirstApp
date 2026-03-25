bc888dbb86077e0000a33c84d23a7689f5bbbc98
Jennifer Miranda <jennifermiranda1245@gmail.com>
2026-03-25 12:44:09 -0500

    Add files via upload

---
+++ FirstSwiftUI/FirstSwiftUI/FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by Miranda-Flores, Jennifer on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
